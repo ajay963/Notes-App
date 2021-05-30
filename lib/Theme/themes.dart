df808f920f70cafddf4f995d786cb8dc9e588d51
Ajay <ajay963@outlook.com>
2021-05-30 09:10:15 +0530

    added light theme data and backend stuff

--- lib/Theme/themes.dart
+++ lib/Theme/themes.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:notes_app/Theme/colors.dart';
 
 class PurpleTheme {
-  static ThemeData lightThme = ThemeData();
+  static ThemeData lightThme = ThemeData(
+      backgroundColor: kLightBG,
+      cardColor: kLightCard,
+      iconTheme: IconThemeData(color: kLightBG, size: 24),
+      textTheme: TextTheme(
+          headline1: TextStyle(
+              fontFamily: GoogleFonts.alef().fontFamily,
+              fontSize: 28,
+              color: kUltraDarkGrey),
+          headline2: TextStyle(
+            fontFamily: GoogleFonts.roboto().fontFamily,
+            fontSize: 24,
+            color: kUltraDarkGrey,
+          ),
+          headline3: TextStyle(
+              fontFamily: GoogleFonts.alef().fontFamily,
+              fontSize: 20,
+              color: kMediumGrey),
+          headline4: TextStyle(
+              fontFamily: GoogleFonts.roboto().fontFamily,
+              fontSize: 18,
+              color: kUltraDarkGrey),
+          caption: TextStyle(
+              fontFamily: GoogleFonts.alef().fontFamily,
+              color: kUltraLightGrey,
+              fontSize: 14),
+          bodyText1: TextStyle(
+              fontFamily: GoogleFonts.roboto().fontFamily,
+              fontSize: 16,
+              color: kMediumGrey),
+          bodyText2: TextStyle(
+              fontFamily: GoogleFonts.alef().fontFamily,
+              fontSize: 16,
+              color: kLightBG)));
   static ThemeData darkTheme = ThemeData();
 }
--- lib/backend/dataStruture/notes.dart
+++ lib/backend/dataStruture/notes.dart
@@ -1,8 +1,50 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
 
 class Notes {
   final String title;
   final String notes;
-  final Timestamp date;
+  final DateTime date;
   final String category;
+  Notes({
+    required this.title,
+    required this.notes,
+    required this.date,
+    required this.category,
+  });
+
+  Notes copyWith({
+    String? title,
+    String? notes,
+    DateTime? date,
+    String? category,
+  }) {
+    return Notes(
+      title: title ?? this.title,
+      notes: notes ?? this.notes,
+      date: date ?? this.date,
+      category: category ?? this.category,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'notes': notes,
+      'date': date.millisecondsSinceEpoch,
+      'category': category,
+    };
+  }
+
+  factory Notes.fromMap(Map<String, dynamic> map) {
+    return Notes(
+      title: map['title'],
+      notes: map['notes'],
+      date: DateTime.fromMillisecondsSinceEpoch(map['date']),
+      category: map['category'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Notes.fromJson(String source) => Notes.fromMap(json.decode(source));
 }
